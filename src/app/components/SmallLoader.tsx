8d6a9bd49187471d453f09c02cd73dce10a150d6
sharmaHarshit2000 <harshitsharma9989@gmail.com>
2025-08-31 18:19:52 +0530

    Loader addded

---
+++ src/app/components/SmallLoader.tsx
@@ -0,0 +1,5 @@
+export default function SmallLoader() {
+  return (
+    <div className="ml-2 inline-block animate-spin rounded-full h-5 w-5 border-2 border-blue-500 border-t-transparent"></div>
+  );
+}
